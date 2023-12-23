13deedd3a68c081b68ea0838325050e4c73c582c
Krishna Pandey <krish36pandey@gmail.com>
2023-12-23 22:09:31 +0530

    added getOne function based on given filter

--- src/repositories/friends-repository.js
+++ src/repositories/friends-repository.js
@@ -2,6 +2,7 @@ const { friend } = require("./../models/index");
 const ApiError = require("./../utils/errors/ApiError");
 const errors = require("./../utils/errors/errors");
 const { statusCodes } = require("./../utils/errors/errors");
+const { Op } = require("sequelize");
 class FriendRepository {
   static async create(data) {
     try {
@@ -14,6 +15,17 @@ class FriendRepository {
         statusCodes.ServerError,
         errors.ServerError
       );
+    }
+  }
+  static async getOne(filterObj) {
+    try {
+      const result = await friend.findOne({
+        where: filterObj,
+      });
+
+      return result;
+    } catch (err) {
+      throw err;
     }
   }
 }
