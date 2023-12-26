506ec1745759df33467bab7f8fb6e418a87acfd1
Krishna Pandey <krish36pandey@gmail.com>
2023-12-26 20:16:10 +0530

    added model for managing requests

---
+++ src/models/requests.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Requests extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Requests.init(
+    {
+      from: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "User",
+          key: "id",
+        },
+      },
+      to: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "User",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Requests",
+    }
+  );
+  return Requests;
+};
