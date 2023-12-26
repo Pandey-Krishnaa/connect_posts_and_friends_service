c70a9c65ac1347d1cadea402770e07e3bdffdb1b
Krishna Pandey <krish36pandey@gmail.com>
2023-12-26 20:15:28 +0530

    added middleware for authentication with auth service

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,38 @@
+const { GET_USER_BY_ID_URL } = require("../config");
+const ApiError = require("../utils/errors/ApiError");
+const { statusCodes, errors } = require("../utils/errors/errors");
+
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.headers["x-auth-token"];
+    if (!token)
+      return next(
+        new ApiError(
+          "login again",
+          statusCodes.UnauthorizedRequest,
+          errors.UnauthorizedRequest
+        )
+      );
+    const response = await fetch(`${GET_USER_BY_ID_URL}/profile/me`, {
+      method: "GET",
+      headers: {
+        "x-auth-token": token,
+      },
+    });
+    if (!response.ok)
+      return next(
+        new ApiError(
+          "login again",
+          statusCodes.UnauthorizedRequest,
+          errors.UnauthorizedRequest
+        )
+      );
+    const data = await response.json();
+    req.user = data.user;
+  } catch (err) {
+    next(err);
+  }
+  next();
+};
+
+module.exports = isAuthenticated;
