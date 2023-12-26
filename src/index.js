b2b21368a1be6845caf7fc637556d79800852997
Krishna Pandey <krish36pandey@gmail.com>
2023-12-26 20:18:20 +0530

    added endpoint for managing requests

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const { PORT } = require("./config/index");
-const { FriendRoutes } = require("./routes/v1/index");
+const { FriendRoutes, RequestRoutes } = require("./routes/v1/index");
 const errorHandler = require("./utils/commons/errorHandler");
+
 app.use("/api/v1/friends", FriendRoutes);
+app.use("/api/v1/requests", RequestRoutes);
 app.all("*", (req, res) => {
   res.status(400).json({
     success: false,
